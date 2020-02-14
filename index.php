3aafc0a589dcae1f60a9e8a017c81cede29465cb
Flammen-werfer-42 <61033635+Flammen-werfer-42@users.noreply.github.com>
2020-02-13 19:00:27 -0600

    Create index.php
    The code was a formless void and darkness covered the face of the deep, while a wind from which the Programmer swept over the face of the waters.
    
    Then Programmer said, "Let there be light"; and there was light

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+        mysql_connect("localhost", "username", "password");
+        mysql_select_db("database_name");
+        mysql_query("CREATE TABLE, ALTER TABLE, SELECT, ETC");
+
+        $result = mysql_query("SELECT * FROM TABLE_NAME");
+
+?>
